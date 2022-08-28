22dfc2c48f758b060e983035cae20362e682818e
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-08-28 17:30:33 +0530

    Create odd_even_negative.c

---
+++ Question-16/odd_even_negative.c
@@ -0,0 +1,31 @@
+/*
+    Write a C program to accept 10 values in an integer array. Display the number of odd, even, and negative numbers.
+*/
+#include<stdio.h>
+int main()
+{
+    int even=0,odd=0,neg=0,i;
+    printf("Enter 10 Numbers : ");
+    int arr[10];
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]<0)
+        {
+            neg++;
+        }
+        if(arr[i]%2==0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+        
+    }
+    printf("No. of Even Numbers : %d\n",even);
+    printf("No. of Odd  Numbers : %d\n",odd);
+    printf("No. of Negative Numbers : %d\n",neg);
+    return 0 ;
+}
