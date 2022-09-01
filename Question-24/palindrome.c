eaca1135a2577e09da73a9907886f198da9c57ed
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-09-01 15:51:20 +0530

    Create palindrome.c

---
+++ Question-24/palindrome.c
@@ -0,0 +1,33 @@
+/*
+    Write a C program to check whether a given string is palindrome or not.
+*/
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n,i,j,flag=0;
+    char str1[20];
+    printf("Enter the String : ");
+    scanf("%s",str1);
+    int l1=strlen(str1);
+    i=0,j=l1-1;
+    while(i<j)
+    {
+        if(str1[i]!=str1[j])
+        {
+            flag=1;
+            break;
+        }
+        i++;
+        j--;
+    }
+    if(flag==0)
+    {
+        printf("String is Palindrome\n");
+    }
+    else
+    {
+        printf("String is not Palindrome\n");
+    }
+    return 0 ;
+}
